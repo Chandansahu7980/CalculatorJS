acd6516676403cbc916d2f358b44d3213588d153
Chandan Kumar Sahu <chandansahu7980@gmail.com>
2025-06-01 16:10:55 +0530

    result area changed to textarea for better wat to display result like line wrap

--- script.js
+++ script.js
@@ -4,47 +4,72 @@ let currentInput = '';
 
 button_numbers.forEach(button => {
     button.addEventListener('click', () => {
-        const value = button.textContent;
-    
+        const value = button.innerHTML;
         currentInput += value;
-        resultSection.textContent = currentInput;
+        resultSection.innerHTML = currentInput;
     });
 });
 
 document.getElementById("c").addEventListener("click", () => {
     currentInput = '';
-    resultSection.textContent = currentInput;
+    resultSection.innerHTML = currentInput;
+});
+
+document.getElementById("percent").addEventListener("click", () => {
+
+    try {
+        // Evaluate the current input and divide by 100
+        const value = eval(currentInput);
+        currentInput = (value / 100).toString();
+    } catch (error) {
+        currentInput = 'Expression Error';
+    }
+    resultSection.innerHTML = currentInput;
 });
 
 document.getElementById("divide").addEventListener("click", () => {
-    currentInput += '/';
-    resultSection.textContent = currentInput;
+    addOperator('/');
+    resultSection.innerHTML = currentInput;
 });
 
 document.getElementById("multiply").addEventListener("click", () => {
-    currentInput += '*';
-    resultSection.textContent = currentInput;
+    addOperator('*');
+    resultSection.innerHTML = currentInput;
 });
 document.getElementById("minus").addEventListener("click", () => {
-    currentInput += '-';
-    resultSection.textContent = currentInput;
+    addOperator('-');
+    resultSection.innerHTML = currentInput;
 });
 
 document.getElementById("plus").addEventListener("click", () => {
-    currentInput += '+';
-    resultSection.textContent = currentInput;
+    addOperator('+');
+    resultSection.innerHTML = currentInput;
 });
 
 document.getElementById("delete").addEventListener("click", () => {
     currentInput = currentInput.slice(0, -1);
-    resultSection.textContent = currentInput;
+    resultSection.innerHTML = currentInput;
 });
 
 document.getElementById("equals").addEventListener("click", () => {
     try {
         currentInput = eval(currentInput).toString();
     } catch (error) {
-        currentInput = 'Error';
+        currentInput = 'Expression Error';
     }
-    resultSection.textContent = currentInput;
+    resultSection.innerHTML = currentInput;
 });
+
+function addOperator(op) {
+    if (currentInput.length === 0) return; // Prevent starting with operator
+
+    const lastChar = currentInput.slice(-1);
+    if (['+', '-', '*', '/','%'].includes(lastChar)) {
+        currentInput = currentInput.slice(0, -1) + op;
+    } else {
+        currentInput += op;
+    }
+
+    // Update the result section with the current input
+    resultSection.innerHTML = currentInput;
+}
