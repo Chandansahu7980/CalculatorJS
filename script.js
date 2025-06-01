fc49386847da3b5b48a647f8ea41212a9e258733
Chandan Kumar Sahu <chandansahu7980@gmail.com>
2025-06-01 15:16:44 +0530

    function added for icons as well

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const buttons = document.querySelectorAll('.btn');
+const button_numbers = document.querySelectorAll('.btn-num');
 const resultSection = document.getElementById('result');
 let currentInput = '';
 
-buttons.forEach(button => {
+button_numbers.forEach(button => {
     button.addEventListener('click', () => {
         const value = button.textContent;
-
-        if (value === 'C') {
-            currentInput = '';
-        } else if (value === '=') {
-            try {
-                currentInput = eval(currentInput).toString();
-            } catch (error) {
-                currentInput = 'Error';
-            }
-        } else {
-            currentInput += value;
-        }
-
+    
+        currentInput += value;
         resultSection.textContent = currentInput;
     });
+});
+
+document.getElementById("c").addEventListener("click", () => {
+    currentInput = '';
+    resultSection.textContent = currentInput;
+});
+
+document.getElementById("divide").addEventListener("click", () => {
+    currentInput += '/';
+    resultSection.textContent = currentInput;
+});
+
+document.getElementById("multiply").addEventListener("click", () => {
+    currentInput += '*';
+    resultSection.textContent = currentInput;
+});
+document.getElementById("minus").addEventListener("click", () => {
+    currentInput += '-';
+    resultSection.textContent = currentInput;
+});
+
+document.getElementById("plus").addEventListener("click", () => {
+    currentInput += '+';
+    resultSection.textContent = currentInput;
+});
+
+document.getElementById("delete").addEventListener("click", () => {
+    currentInput = currentInput.slice(0, -1);
+    resultSection.textContent = currentInput;
+});
+
+document.getElementById("equals").addEventListener("click", () => {
+    try {
+        currentInput = eval(currentInput).toString();
+    } catch (error) {
+        currentInput = 'Error';
+    }
+    resultSection.textContent = currentInput;
 });
